b4f4aea214e6bceb249656ba464fb9dd9a4aea9a
casher0009 <the.casher.uber@gmail.com>
2018-06-28 00:00:07 -0500

    Tercer Commit, ya vamo mejorando chico

--- mainprueba.js
+++ mainprueba.js
@@ -12,10 +12,11 @@ var ctx = canvas.getContext('2d');
 //constants
 var interval;
 var frames = 0;
-var bullets = []
+var score = 0;
 var basuras = []
  var images = {
   hero: 'https://t1.rbxcdn.com/cbc4940cc40fc00da68ffaab0ed89ae1',
+  hero2: 'https://t1.rbxcdn.com/cbc4940cc40fc00da68ffaab0ed89ae1',
   bg:'./images/canvas bg.jpg',
   bullet: './images/bullet.png',
  }
@@ -25,7 +26,7 @@ var imgBasura = {
   botella: './images/botella.png',
   cigarro: './images/cigarro.png',
   duff: './images/duff.png',
-  popote: './images/duff.png'
+  popote: './images/popote.png'
 }
 
 
@@ -37,6 +38,7 @@ this.x = canvas.width/2
 this.y = canvas.height -64
 this.width = 64;
 this.height = 64
+this.bullets = []
 this.image = new Image();
 this.image.src = images.hero;
 this.image.onload = function(){
@@ -55,6 +57,12 @@ this.image.src = images.bg
 }
 draw(){
 ctx.drawImage(this.image,0,0,canvas.width,canvas.height);
+
+ctx.fillText("You Score: "+ score ,100,40)
+ctx.fillStyle = "white";
+ctx.font = '55px Avenir';
+
+
 }
 }
 
@@ -65,12 +73,12 @@ this.height = 96
 this.x = character.x + character.width /2 - this.width/2;
 this.y= character.y - this.height
 this.vY = -3;
-this.isTouching= function(item){
-  return  (this.x < item.x + item.width) &&
-          (this.x + this.width > item.x) &&
-          (this.y < item.y + item.height) &&
-          (this.y  - 20 + this.height > item.y);
-                  }      
+// this.isTouching= function(item){
+//   return  (this.x < item.x + item.width) &&
+//           (this.x + this.width > item.x) &&
+//           (this.y < item.y + item.height) &&
+//           (this.y  - 20 + this.height > item.y);
+//                   }      
 this.image = new Image();
 this.image.src = images.bullet;
 this.image.onload = function(){
@@ -90,20 +98,22 @@ this.x = x;
 this.y = y;
 this.width = 64;
 this.height = 64;
+this.live = 1
 this.contador = Math.floor(Math.random()*6);
-this.isTouching= function(item){
-  console.log(item)
-  if((this.x < item.x + item.width) &&
-          (this.x + this.width > item.x) &&
-          (this.y < item.y + item.height) &&
-          (this.y  - 20 + this.height > item.y)){
-            return true
-          };
-  }      
+// this.isTouching= function(item){
+//   console.log(item)
+//   if((this.x < item.x + item.width) &&
+//           (this.x + this.width > item.x) &&
+//           (this.y < item.y + item.height) &&
+//           (this.y  - 20 + this.height > item.y)){
+//             return true
+//           };
+//   }      
 this.image = new Image();
 this.image.src = (this.contador === 1 ) ? imgBasura.bolsa: 
                  (this.contador === 2 ) ? imgBasura.botella:
                  (this.contador === 3 ) ? imgBasura.cigarro:
+                 (this.contador === 4 ) ? imgBasura.duff:
                  (this.contador === 4 ) ? imgBasura.duff:
                  imgBasura.popote;  
 
@@ -112,9 +122,14 @@ this.image.src = (this.contador === 1 ) ? imgBasura.bolsa:
   draw(){
   ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
 }
+isTouching(item){
+  return  (this.x < item.x + item.width) &&
+          (this.x + this.width > item.x) &&
+          (this.y < item.y + item.height) &&
+          (this.y + this.height > item.y);
 
 }
-
+}
 
 //instances
 var hero = new Hero()
@@ -130,7 +145,8 @@ function update(){
   generateBasura()
   drawBasura()
   drawBullets()
-  checkCollition();
+  checkCollition()
+  checkClean()
 }
 
 
@@ -145,7 +161,7 @@ function start(){
 
   //BASURA
   function generateBasura(){
-    if(!(frames%400===0) ) return;
+    if(!(frames%30===0) ) return;
     var basura = new Basura();
     basuras.push(basura)
   }
@@ -156,24 +172,35 @@ function start(){
   })}
 
   function generatebullet(){
+  // if(!(frames%2===0) ) return;
     var bullet = new Bullet(hero);
-    bullets.push(bullet);
+    hero.bullets.push(bullet);
   }
   
   function drawBullets(){
-    bullets.forEach(function(bullet){
+    hero.bullets.forEach(function(bullet){
       bullet.draw()
     })}
 
 
     
   function checkCollition(){
-    basuras.forEach(function (basura){
-      bullets.forEach(function (bullet){
-        console.log("tocaste")
-      })
-    })
-}  
+    for(var i = 0; i<basuras.length; i++)
+    for(var j = 0; j<hero.bullets.length; j++)
+    if(basuras[i].isTouching(hero.bullets[j]))
+    basuras[i].live--;
+  }
+
+  function checkClean(){
+    for(var i = 0; i<basuras.length; i++)
+      for(var j = 0; j<hero.bullets.length; j++)
+        if(basuras[i].live === 0){
+        basuras.splice(i,1);
+        hero.bullets.splice(j,1);
+        score++;
+    }
+  }
+
   
 
 //listeners
@@ -181,11 +208,11 @@ function start(){
 addEventListener('keydown', function(e){
   console.log(e.keyCode)
   switch(e.keyCode){
-    case 37:
+    case 90:
     if(hero.x === 0)return;
     hero.x-=64;
     break;
-    case 39:
+    case 88:
     if(hero.x === canvas.width - hero.width)return;
     hero.x+=64;
     break;
@@ -196,6 +223,3 @@ addEventListener('keydown', function(e){
 })
 
 
-document.getElementById("one").addEventListener('click',function(){
-  if (frames <= 1) start()
-})
--- second.js
+++ second.js
@@ -1,118 +1,212 @@
-//canvas height="576" width="512"
+//canvas2 height="576" width="512"
 var canvas2 = document.getElementById("second");
 var ctx2 = canvas2.getContext('2d');
 
-// // //PRUEBAS Y DIMENSIONES DEL CANVAS
+// // //PRUEBAS Y DIMENSIONES DEL CANVAS2
 // ctx2.fillRect(0,0,50,50)
-// ctx2.fillRect(second.width-50,0,50,50)
-// ctx2.fillRect(second.width-50,second.height-50,50,50)
-// ctx2.fillRect(0,second.height-50,50,50)
+// ctx2.fillRect(canvas2.width-50,0,50,50)
+// ctx2.fillRect(canvas2.width-50,canvas2.height-50,50,50)
+// ctx2.fillRect(0,canvas2.height-50,50,50)
 
 
 //constants
 var interval2;
 var frames2 = 0;
-var enemies2 = []
- var images2 = {
-  hero2: 'https://t1.rbxcdn.com/cbc4940cc40fc00da68ffaab0ed89ae1',
-  enemy2: 'https://banner2.kisspng.com/20180525/gsh/kisspng-super-mario-bros-goomba-rosalina-8-bit-5b080340880705.2764707415272517765572.jpg',
-  bg2:'./images/canvas bg.jpg',
-  bullet2: './images/ScorpionSpear_3Danim.gif'
- }
+var score2 = 0;
+var basuras2 = []
+//  var images = {
+//   hero: 'https://t1.rbxcdn.com/cbc4940cc40fc00da68ffaab0ed89ae1',
+//   bg:'./images/canvas2 bg.jpg',
+//   bullet2: './images/bullet2.png',
+//  }
 
+// var imgBasura2 = {
+//   bolsa:'./images/bolsa.png',
+//   botella: './images/botella.png',
+//   cigarro: './images/cigarro.png',
+//   duff: './images/duff.png',
+//   popote: './images/popote.png'
+// }
 
 //class
-class Character2{
+
+class Hero2{
   constructor(x=0,y=0,img){
-this.x = x
-this.y = y
+this.x = canvas2.width/2
+this.y = canvas2.height -64
 this.width = 64;
 this.height = 64
+this.bullets2 = []
 this.image = new Image();
-this.image.src = img;
+this.image.src = images.hero2;
 this.image.onload = function(){
   this.draw()
 }.bind(this);
-this.vX = 1
-this. vY = 1
   }
   draw(){
     ctx2.drawImage(this.image,this.x,this.y,this.width,this.height)
   }
 }
 
-class HERO2 extends Character2{
-  constructor(x,y,img){
-    super(x,y,img)
-    this.bullets2 = []
-  }
+class Board2{
+  constructor(){
+this.image = new Image();
+this.image.src = images.bg
+}
+draw(){
+ctx2.drawImage(this.image,0,0,canvas2.width,canvas2.height);
+
+ctx2.fillText("You Score: "+ score2 ,100,40)
+ctx2.fillStyle = "white";
+ctx2.font = '55px Avenir';
+
+
+}
 }
 
-    class Board2{
-      constructor(){
-        this.image = new Image();
-        this.image.src = images2.bg2
-        this.image.onload = function(){
-          this.draw();
-        }.bind(this)
-      }
-        draw(){
-          ctx2.drawImage(this.image,0,0,canvas.width,canvas.height)
-        }
-      }
-    
-      class Bullet2{
-        constructor(character2){
-          this.width = 20
-          this.height = 20
-            this.x = character2.x + character2.width /2 - this.width/2;
-            this.y= character2.y - this.height
-            this.vY = -1
-            this.image = new Image();
-            this.image.src = images2.bullet2;
-            this.image.onload = function(){
-              this.draw();
-            }.bind(this)
-          }//END OF CONSTRUCTOR
-      draw(){
-        this.y+=this.vY;
-        ctx2.drawImage(this.image, this.x, this.y, this.width, this.height)
-      }
-        }
+class Bullet2{
+constructor(character2){
+this.width = 35
+this.height = 96
+this.x = character2.x + character2.width /2 - this.width/2;
+this.y= character2.y - this.height
+this.vY = -3;
+// this.isTouching= function(item){
+//   return  (this.x < item.x + item.width) &&
+//           (this.x + this.width > item.x) &&
+//           (this.y < item.y + item.height) &&
+//           (this.y  - 20 + this.height > item.y);
+//                   }      
+this.image = new Image();
+this.image.src = images.bullet;
+this.image.onload = function(){
+this.draw();
+}.bind(this)
+}
+  draw(){
+  this.y+=this.vY;
+  ctx2.drawImage(this.image, this.x, this.y, this.width, this.height)
+}
+
+}
+
+class Basura2{
+  constructor(x=Math.floor((Math.random())*8)*64,y=(Math.floor((Math.random())*3)*64)+64 ){
+this.x = x;
+this.y = y;
+this.width = 64;
+this.height = 64;
+this.live = 1
+this.contador = Math.floor(Math.random()*6);
+// this.isTouching= function(item){
+//   console.log(item)
+//   if((this.x < item.x + item.width) &&
+//           (this.x + this.width > item.x) &&
+//           (this.y < item.y + item.height) &&
+//           (this.y  - 20 + this.height > item.y)){
+//             return true
+//           };
+//   }      
+this.image = new Image();
+this.image.src = (this.contador === 1 ) ? imgBasura.bolsa: 
+                 (this.contador === 2 ) ? imgBasura.botella:
+                 (this.contador === 3 ) ? imgBasura.cigarro:
+                 (this.contador === 4 ) ? imgBasura.duff:
+                 (this.contador === 4 ) ? imgBasura.duff:
+                 imgBasura.popote;  
+
+                
+}
+  draw(){
+  ctx2.drawImage(this.image, this.x, this.y, this.width, this.height)
+}
+isTouching(item){
+  return  (this.x < item.x + item.width) &&
+          (this.x + this.width > item.x) &&
+          (this.y < item.y + item.height) &&
+          (this.y + this.height > item.y);
+
+}
+}
 
 //instances
-var hero2 = new HERO2(canvas.width/2, canvas.height -60, images.hero)
+var hero2 = new Hero2()
 var board2 = new Board2()
 
 
 //mainFunctions
-
 function update2(){
-  ctx2.clearRect(0,0,canvas.width,canvas.height)
+  frames2++;
+  ctx2.clearRect(0,0,canvas2.width,canvas2.height)
   board2.draw()
   hero2.draw()
+  generateBasura2()
+  drawBasura2()
   drawBullets2()
+  checkCollition2()
+  checkClean2()
 }
+
+
 function start2(){
-interval2 = setInterval(update2,1000/60)
-}
+  interval2 = setInterval(function(){
+    update2();
+  },1000/60)
+  }
+
+
+  //aux functions
+
+  //BASURA2
+  function generateBasura2(){
+    if(!(frames2%30===0) ) return;
+    var basura2 = new Basura2();
+    basuras2.push(basura2)
+  }
+
+  function drawBasura2(){
+    basuras2.forEach(function(basura2){
+      basura2.draw();
+  })}
+
+  function generatebullet2(){
+  // if(!(frames2%2===0) ) return;
+    var bullet2 = new Bullet2(hero2);
+    hero2.bullets2.push(bullet2);
+  }
+  
+  function drawBullets2(){
+    hero2.bullets2.forEach(function(bullet2){
+      bullet2.draw()
+    })}
+
+
+    
+  function checkCollition2(){
+    for(var i = 0; i<basuras2.length; i++)
+    for(var j = 0; j<hero2.bullets2.length; j++)
+    if(basuras2[i].isTouching(hero2.bullets2[j]))
+    basuras2[i].live--;
+  }
+
+  function checkClean2(){
+    for(var i = 0; i<basuras2.length; i++)
+      for(var j = 0; j<hero2.bullets2.length; j++)
+        if(basuras2[i].live === 0){
+        basuras2.splice(i,1);
+        hero2.bullets2.splice(j,1);
+        score2++;
+    }
+  }
 
 
 
 
-//aux functions
-function generatebullet2(){
-  var bullet2 = new Bullet2(hero2);
-  hero2.bullets2.push(bullet2);
-}
 
-function drawBullets2(){
-  hero2.bullets2.forEach(function(bullet2){
-    bullet2.draw()
-  });
-}
-
-
+  function OnePlayer(){
+    document.getElementById('second').style.visibility = "hidden"
+  }
+  
 
 //listeners
 
@@ -127,13 +221,17 @@ addEventListener('keydown', function(e){
     if(hero2.x === canvas2.width - hero2.width)return;
     hero2.x+=64;
     break;
-    case 32:
+    case 13:
       generatebullet2()
       break
   }
 })
 
+document.getElementById("one").addEventListener('click',function(){
+  if (frames <= 1) start(); OnePlayer()
+})
 
-
-
-start2();
+document.getElementById("two").addEventListener('click',function(){
+  if (frames <= 1) start()
+  ;if (frames2 <= 1) start2()
+})
